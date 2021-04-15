387ee1c91b8b7fc0533e8ac64d4ac02668b9fae1
Oliwier965 <olikucz@vp.pl>
2021-04-15 17:43:09 +0200

    Scroll animation overload |Break| |Part1|

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function animatedSlides() {
   const nav = document.querySelector(".nav-header");
   //Loop over each slide
   sliders.forEach((slide) => {
-    const revealImg = document.querySelector(".reveal-img");
+    const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
     //GSAP
@@ -21,6 +21,18 @@ function animatedSlides() {
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.55");
     slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.7");
     //Create Scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: "#fff",
+        colorTrigger: "#fff",
+        name: "slide",
+      })
+      .addTo(controller);
   });
 }
 
